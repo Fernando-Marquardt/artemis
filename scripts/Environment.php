55798fb4b05c62412c66e3401b4df4cde4b5cd55
Fernando Marquardt <fernando.marquardt@gmail.com>
2019-09-15 14:51:52 -0300

    Added DotEnv to init environment on dev.

---
+++ scripts/Environment.php
@@ -0,0 +1,14 @@
+<?php
+namespace Artemis\Scripts;
+
+use Dotenv\Dotenv;
+
+class Environment {
+
+    public static function setEnvironment(): void {
+        $dotenv = Dotenv::create(__DIR__ . '/../');
+        $dotenv->load();
+
+        $dotenv->required(['DATABASE_HOST', 'DATABASE_USER', 'DATABASE_PASS', 'DATABASE_NAME']);
+    }
+}
