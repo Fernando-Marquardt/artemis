048b3a36b2558dc11fac858613d37df12de6a424
Fernando Marquardt <fernando.marquardt@gmail.com>
2019-09-15 18:07:24 -0300

    Added Symfony's translation component.

--- core/config.php
+++ core/config.php
@@ -1,32 +1,49 @@
 <?php
-use Slim\Views\Twig;
-use Twig\TwigFunction;
 use Artemis\Engine\Database;
+use Slim\Views\Twig;
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Translator;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 return [
     'database.host' => DI\env('DATABASE_HOST'),
     'database.user' => DI\env('DATABASE_USER'),
     'database.pass' => DI\env('DATABASE_PASS'),
     'database.name' => DI\env('DATABASE_NAME'),
+
+    'locale.default' => 'en_US',
+
     Database::class => DI\create()
         ->constructor(DI\get('database.host'), DI\get('database.user'), DI\get('database.pass'), DI\get('database.name'))
         ->method('connect'),
+
+    Translator::class => DI\factory(function($defaultLocale) {
+        // TODO We need load the user locale when logged in.
+        $translator = new Translator($defaultLocale);
+        $translator->setFallbackLocales([ $defaultLocale ]);
+        $translator->addLoader('php', new PhpFileLoader());
+
+        // Add the translations to the translator resources
+        $translator->addResource('php', __DIR__ . '/../translations/messages.en_US.php', 'en_US', 'messages+intl-icu');
+
+        return $translator;
+    })
+        ->parameter('defaultLocale', DI\get('locale.default')),
     
-    Twig::class => function() {
-        global $imgs, $fimgs, $title, $lang, $announcement; // Needed for now
+    Twig::class => function(Translator $translator) {
+        global $imgs, $fimgs, $announcement; // Needed for now
 
         $view = new Twig(__DIR__ . '/../templates');
         $environment = $view->getEnvironment();
 
+        $environment->addFilter(new TwigFilter('trans', [$translator, 'trans']));
+
         $environment->addGlobal('images_url', "/$imgs$fimgs");
-        $environment->addGlobal('page_name', $title);
-        $environment->addGlobal('lang', $lang);
         $environment->addGlobal('announcement', $announcement);
 
         $environment->addFunction(new TwigFunction('logo', function() {
-            global $title;
-
-            logo($title);
+            logo(null);
         }));
 
         $environment->addFunction(new TwigFunction('menu_up', function() {
---
+++ translations/messages.en_US.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'game' => [
+        'title' => 'Artemis'
+    ],
+
+    'home' => [
+        'title' => 'Home',
+        'total_users' => 'We have a total of {count} users.',
+        'past_hours' => '{count} of them have been active for the past 48 hours.',
+        'choose_language' => 'Choose language:'
+    ]
+];
